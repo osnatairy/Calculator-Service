19000171a3d437d90773096aa0cedfcedc851d35
Osnat Drien <osnat.drain@biu.ac.il>
2018-06-15 11:31:14 +0300

    update tet files

--- app/server.js
+++ app/server.js
@@ -3,20 +3,18 @@ var express = require("express");
 var app = express();
 var bodyParser = require('body-parser');
 var calc = require("./calculate");
-//app.use(bodyParser())
+
+
 app.use(express.json()) 
 app.use(bodyParser.urlencoded({ extended: false })); // for parsing application/x-www-form-urlencoded
-//app.use(bodyParser.json()); // for parsing application/json
-// parse various different custom JSON types as JSON
-//app.use(bodyParser.json({ type: 'application/*+json' }))
+
 
 module.exports = app;
 
 app.post("/calculate", function(req, res) {
     //get the variables from the url query
     console.log("*******");
-    console.log(Object.keys(req.body));
-    console.log(Object.values(req.body));
+    
     let input = req.body.input;
     let calculatorState = req.body.calculatorState;
 
